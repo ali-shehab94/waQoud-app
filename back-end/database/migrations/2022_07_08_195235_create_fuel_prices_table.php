18b74d395e7b1fcc1b33e631e2581b34cad01917
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-08 23:03:54 +0300

    Back end: Create tables 'fuel types, fuel prices, vehicles' and their models

---
+++ back-end/database/migrations/2022_07_08_195235_create_fuel_prices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuelPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fuel_prices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('fuel_type');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuel_prices');
+    }
+}
---
+++ back-end/database/migrations/2022_07_08_195441_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('model_name');
+            $table->integer('model_year');
+            $table->integer('mileage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
