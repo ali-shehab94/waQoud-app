728351525e6ea7cc4a71a079b850a7ee4619150e
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-08 22:32:59 +0300

    Back end: Create tables 'users, brakes, suspensions, engine oils, wheels' and their models

---
+++ back-end/app/Models/EngineOil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EngineOil extends Model
+{
+    use HasFactory;
+    protected $table = 'engine_oils';
+
+}
