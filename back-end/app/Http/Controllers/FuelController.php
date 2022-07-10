13fe456c3400d48f180a52690131e98540388340
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-10 23:50:44 +0300

    Back end: Add goutte scraper to Fuel controller to get latest gas prices

--- back-end/app/Http/Controllers/FuelController.php
+++ back-end/app/Http/Controllers/FuelController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Goutte\Client;
 use App\Models\FuelType;
 use App\Models\FuelPrice;
 
@@ -20,6 +20,31 @@ class FuelController extends Controller
             "UNL95" => $UNL95_price,
             "UNL98" => $UNL98_price,
             "Diesel" => $Diesel_price,
+        ], 200);
+    }
+
+
+
+    public function scrapeFuelPrices()
+    {
+        //create a goutte client
+        $client = new Client();
+        $url = 'https://www.iptgroup.com.lb/ipt/en/our-stations/fuel-prices';
+        $page = $client->request('GET', $url);
+        $rawData = $page->filter('.pricesTable')->text();
+        $myArray = explode(' ', $rawData);
+        $UNL95 = $myArray[2];
+        $prices = array(
+            'UNL_95' => $myArray[2],
+            'UNL_95_difference' => $myArray[4],
+            'UNL_98' => $myArray[8],
+            'UNL_98_difference' => $myArray[10],
+            'Diesel' => $myArray[13],
+            'Diesel difference' => $myArray[15],
+        );
+        return response()->json([
+            "status" => "success",
+            "prices" => $prices
         ], 200);
     }
 }
--- back-end/config/app.php
+++ back-end/config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Weidner\Goutte\GoutteServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -229,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Goutte' => Weidner\Goutte\GoutteFacade::class,
 
     ],
 
