74ea5802f8b521586509048feed9f290f6645a8b
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-13 21:03:20 +0300

    Back end: Update user model with jwt identifier and cusom claims

--- back-end/app/Models/User.php
+++ back-end/app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
