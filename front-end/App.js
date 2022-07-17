5d62e492dcb9d75a9718201127831df127cbb609
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-17 21:10:29 +0300

    Front end: Create login screen

--- front-end/App.js
+++ front-end/App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 //screens
-// import { Login } from './src/screens/Login';
+import { Login } from './src/screens/Login';
 import { Welcome } from './src/screens/Welcome';
 
 export default function App() {
-    return <Welcome />;
+    return <Login />;
 }
--- front-end/src/screens/Login.js
+++ front-end/src/screens/Login.js
@@ -1,22 +1,59 @@
-// import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-// export const Login = () => {
-//     return (
-//         <View style={styles.container}>
-//             <Text style={styles.title}>Sing in</Text>
-//         </View>
-//     );
-// };
+export const Login = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Sing in</Text>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder='Email' style={styles.textInput}></TextInput>
+                <TextInput placeholder='Password' style={styles.textInput}></TextInput>
+                <TouchableHighlight style={styles.googleButton}>
+                    <Text>
+                        <AntDesign name='google' size={24} color='black' />
+                        Continue with google
+                    </Text>
+                </TouchableHighlight>
+            </View>
+        </View>
+    );
+};
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#BE1931',
-//         alignItems: 'center',
-//     },
-//     title: {
-//         marginTop: '20%',
-//         color: '#0F5F53',
-//         fontSize: '40%',
-//     },
-// });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E9E9E9',
+        alignItems: 'center',
+        paddingTop: 40,
+    },
+    title: {
+        marginTop: 25,
+        color: '#0F5F53',
+        fontSize: 40,
+    },
+    inputContainer: {
+        marginTop: 30,
+        alignItems: 'center',
+    },
+    textInput: {
+        margin: 10,
+        width: 250,
+        height: 52,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        borderColor: 'black',
+        borderWidth: 1.5,
+    },
+    googleButton: {
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        width: 250,
+        height: 52,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        borderColor: 'black',
+        borderWidth: 1.5,
+    },
+});
