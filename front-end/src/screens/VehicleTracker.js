8e9eb37084cfe7527249d38c3464c5080014a0ab
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-07-25 17:20:42 +0300

    Front end: Create vehicle tracker component

--- front-end/src/screens/VehicleTracker.js
+++ front-end/src/screens/VehicleTracker.js
@@ -1,9 +1,30 @@
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { UserContext } from '../../context/UserContext';
+import { useState, useEffect, useContext } from 'react';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export const VehcileTracker = () => {
+    const [user, setUser] = useContext(UserContext);
+    console.log(user.selectedVehicle);
+
     return (
         <View style={styles.container}>
-            <View></View>
+            <View style={styles.header}>
+                <Text>{user.selectedVehicle}</Text>
+            </View>
+            <View>
+                <View style={styles.tracker}>
+                    <View style={styles.trackerTitle}>
+                        <FontAwesome5 name='oil-can' size={40} color='black' />
+                        <Text>Engine Oil</Text>
+                    </View>
+                    <View>
+                        <Text>Type:</Text>
+                        <Text>Last replaced:</Text>
+                        <Text>Replace at:</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 };
@@ -12,5 +33,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#E9E9E9',
+    },
+    header: {
+        backgroundColor: '#0F5F53',
+        width: '90%',
+        height: '7%',
+        margin: '12%',
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    tracker: {
+        backgroundColor: '#D9D9D9',
+        alignSelf: 'center',
+        width: '90%',
+        height: '35%',
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 5,
+    },
+    trackerTitle: {
+        paddingHorizontal: 20,
     },
 });
