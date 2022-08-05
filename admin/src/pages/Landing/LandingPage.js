1d171b5abc35a5b949c73e9f624ce51ede8d8282
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-08-05 21:31:25 +0300

    Admin: Create sign in page

--- admin/src/pages/Landing/LandingPage.js
+++ admin/src/pages/Landing/LandingPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LandingPage.css';
 import Header from '../../components/Header';
--- admin/src/pages/Login/Login.js
+++ admin/src/pages/Login/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import Header from '../../components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    let navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = async (e) => {
+        e.preventDefault();
+        axios({
+            method: 'POST',
+            url: 'jdjnsjdcn',
+            headers: { 'content-type': 'multipart/form-data' },
+        })
+            .then((response) => {
+                if (response.data.user_type === 'admin') {
+                    localStorage.setItem('token', response.data.token);
+                    localStorage.setItem('name', response.data.user_name);
+                    navigate('/Main');
+                } else {
+                    alert('User is not an admin');
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <div className='container'>
-            <h1>Login</h1>
+            <div className='page-container'>
+                <Header
+                    text='Login'
+                    onClick={() => {
+                        navigate('/login');
+                    }}
+                />
+                <div className='content-container'>
+                    <div className='sign-in-card'>
+                        <div className='form'>
+                            <h1>Sign In</h1>
+                            <div className='inputs'>
+                                <input onChange={(e) => setEmail(e.target.value)} className='input' placeholder='email'></input>
+                                <input onChange={(e) => setPassword(e.target.value)} className='input' placeholder='password' type={'password'}></input>
+                                <button className='buttonAuth'>Sign In</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
