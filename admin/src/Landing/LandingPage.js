13220a8e3e3e81b8f543f21b13ff9725efd6b4a8
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-08-05 19:57:46 +0300

    Admin: Style landing page

--- admin/src/Landing/LandingPage.js
+++ admin/src/Landing/LandingPage.js
@@ -10,13 +10,14 @@ function LandingPage() {
         <div className='page-container'>
             <div className='nav-container'>
                 <div className='nav-bar'>
-                    <h3 className='nav-text'>Login as admin</h3>
+                    <h3 className='nav-text'>Admin Login</h3>
                     <img src={require('../assets/logos/no-border.png')} className='nav-logo' />
                 </div>
             </div>
             <div className='content-container'>
                 <div className='waqoud-info'>
                     <h1>Welcome to waqoud.com</h1>
+                    <p>waQoud is a utility app that calculates your trip costs and keeps you updated on the daily fluctuating gas prices.</p>
                 </div>
                 <div>
                     <img className='logo-main' src={require('../assets/logos/logo-main.png')} />
