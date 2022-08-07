7597503407dda70cc86f908608cbd5adc0e6c622
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-08-07 19:17:23 +0300

    Admin: Fix minor bug get users function

--- admin/src/components/UserVehiclesModal/UserVehiclesModal.js
+++ admin/src/components/UserVehiclesModal/UserVehiclesModal.js
@@ -13,22 +13,26 @@ function UserVehiclesModal({ data, clearData }) {
                     <h1>User Vehicles</h1>
                 </div>
                 <div className='vehicles-container'>
-                    {data
-                        ? data.map((vehicle) => (
-                              <div className='display-user-vehicles' key={vehicle[0].id}>
-                                  <h4 className='vehicle'>
-                                      <IoCarSport size={30} />
-                                      {vehicle[0].make.charAt(0).toUpperCase() +
-                                          vehicle[0].make.substring(1) +
-                                          ' ' +
-                                          vehicle[0].model.charAt(0).toUpperCase() +
-                                          vehicle[0].model.substring(1) +
-                                          ' ' +
-                                          vehicle[0].year}
-                                  </h4>
-                              </div>
-                          ))
-                        : null}
+                    {data ? (
+                        data.map((vehicle) => (
+                            <div className='display-user-vehicles' key={vehicle[0].id}>
+                                <h4 className='vehicle'>
+                                    <IoCarSport size={30} />
+                                    {vehicle[0].make.charAt(0).toUpperCase() +
+                                        vehicle[0].make.substring(1) +
+                                        ' ' +
+                                        vehicle[0].model.charAt(0).toUpperCase() +
+                                        vehicle[0].model.substring(1) +
+                                        ' ' +
+                                        vehicle[0].year}
+                                </h4>
+                            </div>
+                        ))
+                    ) : (
+                        <div className='display-user-vehicles'>
+                            <h4 className='vehicle'>User has no vehicles</h4>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
