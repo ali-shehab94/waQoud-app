0bd2b6660ce638a9922901dea5bb2ea665389883
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-08-07 13:51:09 +0300

    Admin: Add close button to modal

--- admin/src/components/UserVehiclesModal/UserVehiclesModal.js
+++ admin/src/components/UserVehiclesModal/UserVehiclesModal.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './UserVehiclesModal.css';
+import { AiOutlineClose } from 'react-icons/ai';
 
 function UserVehiclesModal(props) {
     return (
         <div className='container'>
-            <div className='modal'>UserVehiclesModal</div>
+            <div className='modal'>
+                UserVehiclesModal
+                <div className='close'>
+                    <AiOutlineClose size={30} />
+                </div>
+            </div>
         </div>
     );
 }
